1b68d1bbb61bd2d88c7e17d958fa51aa2f9bbc40
Odubo Timidi James <81032342+timidij@users.noreply.github.com>
2024-03-10 20:41:38 +0100

    Add files via upload

---
+++ celcius.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+void convert();
+int main(){
+convert();
+return 0;
+}
+
+void convert(){
+
+for(int f= -16; f <= 105; f++){
+        double c;
+    c = 5*(f-32)/9.0;
+    printf("f = %d, c = %f \n", f,c);
+
+}
+}
+
